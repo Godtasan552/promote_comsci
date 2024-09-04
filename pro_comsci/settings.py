149b7874d5f979a5000447af8c8140c643c38fcf
Weerawat Tammung <werawat7903@gmail.com>
2024-09-04 19:28:17 +0700

    set new django

--- pro_comsci/settings.py
+++ pro_comsci/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'pro_comsci.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [Path.joinpath, "templates"],
+        'DIRS': [Path.joinpath(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,10 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR,'static']
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfile_bilud','static')
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_bilud', 'static')
 MEDIA_URL = '/medai/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
