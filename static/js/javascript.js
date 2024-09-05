c78b6fe16ac2669958965cdb5cebdb6f4817b294
Weerawat Tammung <werawat7903@gmail.com>
2024-09-05 23:52:15 +0700

    add navbar abd togle munu scale

---
+++ static/js/javascript.js
@@ -0,0 +1,11 @@
+
+    // ฟังก์ชันสำหรับตรวจสอบการเลื่อนของหน้าเว็บ
+    window.addEventListener('scroll', function() {
+        const navbar = document.getElementById('mainNav');
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+
