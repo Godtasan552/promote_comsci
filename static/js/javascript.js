4daa160870f6347c7d38d3b6b0f34d6859516308
Weerawat Tammung <werawat7903@gmail.com>
2024-09-10 01:16:04 +0700

    work curriculum completed

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -8,8 +8,26 @@ window.addEventListener("scroll", function() {
     }
 });
 
-
+// link sskru
     function redirectTo(url){
         window.location.href = url;
     
     }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        // Check if there's a hash in the URL
+        if (window.location.hash) {
+            const targetId = window.location.hash;
+            const carousel = document.querySelector('#carouselExampleIndicators'); // Update to match your carousel ID
+            const carouselInstance = new bootstrap.Carousel(carousel);
+            const targetSlide = document.querySelector(targetId);
+    
+            if (targetSlide) {
+                const slideIndex = Array.from(carousel.querySelectorAll('.carousel-item')).indexOf(targetSlide);
+                if (slideIndex !== -1) {
+                    carouselInstance.to(slideIndex);
+                }
+            }
+        }
+    });
+    
