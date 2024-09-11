aa3f52e42a8cea6953829f0a570c33a28f86fe20
deaww77 <126137481+deaww77@users.noreply.github.com>
2024-09-11 12:18:25 +0700

    add vercel file

--- pro_comsci/settings.py
+++ pro_comsci/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-o1k^#i_pm&!qdg^h*@_fd2iz*e121=_+o%6(##6g#jc4s5w-yc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_bilud', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_bilud', 'static')
 MEDIA_URL = '/medai/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
